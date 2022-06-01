b1fd72921d87406d70bfc9a1d0a5db1968163471
oguzky7 <oguzky7@gmail.com>
2022-05-31 21:35:58 -0400

    1st commit

---
+++ src/gitReview/gitReview01.java
@@ -0,0 +1,11 @@
+package gitReview;
+
+public class gitReview01 {
+
+	public static void main(String[] args) {
+
+		System.out.print("today we learn github");
+		System.out.println("today is a very good day");
+
+	}
+}
